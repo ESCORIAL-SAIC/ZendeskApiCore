484a62868c7c6523bb410c5902d94ced4820b9ca
Mauricio Lopez <mlopez@escorial.com.ar>
2025-03-13 17:11:37 -0300

    añadido de clases

---
+++ ZendeskApiCore/Models/Etiqueta.cs
@@ -0,0 +1,11 @@
+﻿namespace ZendeskApiCore.Models
+{
+    public class Etiqueta
+    {
+        public int Numero { get; set; }
+        public Guid ProductoId { get; set; }
+        public string ProductoCodigo { get; set; }
+        public string ProductoNombre { get; set; }
+        public string ProductoTipo { get; set; }
+    }
+}
---
+++ ZendeskApiCore/Models/EtiquetaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ZendeskApiCore.Models
+{
+    public class EtiquetaDto
+    {
+        public int Numero { get; set; }
+        public string ProductoTipo { get; set; }
+    }
+}
