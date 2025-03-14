f1e00f63637b25124604e2e48f0a478592413574
Mauricio Lopez <mlopez@escorial.com.ar>
2025-03-14 08:15:24 -0300

    agregado de endpoint en ProductoController

--- ZendeskApiCore/Models/Etiqueta.cs
+++ ZendeskApiCore/Models/Etiqueta.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ZendeskApiCore.Models
 {
@@ -10,13 +11,19 @@ namespace ZendeskApiCore.Models
     {
         [Column("numero")]
         public int Numero { get; set; }
+        [JsonIgnore]
         [Column("producto_id")]
         public Guid ProductoId { get; set; }
+        [JsonIgnore]
         [Column("producto_c")]
         public string ProductoCodigo { get; set; }
+        [JsonIgnore]
         [Column("producto_n")]
         public string ProductoNombre { get; set; }
+        [JsonIgnore]
         [Column("tipo")]
-        public string ProductoTipo { get; set; }
+        public string ProductoTipoCodigo { get; set; }
+        [NotMapped]
+        public ProductoDto Producto { get; set; }
     }
 }
--- ZendeskApiCore/Models/EtiquetaDto.cs
+++ ZendeskApiCore/Models/EtiquetaDto.cs
@@ -1,8 +1,10 @@
-﻿namespace ZendeskApiCore.Models
+﻿# nullable disable
+
+namespace ZendeskApiCore.Models
 {
     public class EtiquetaDto
     {
         public int Numero { get; set; }
-        public string ProductoTipo { get; set; }
+        public TipoProductoDto ProductoTipo { get; set; }
     }
 }
