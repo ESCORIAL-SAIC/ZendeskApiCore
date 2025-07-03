3fba6a14c5d51a3601fdfea5ee1a28169218882e
maurinicolas <mlopez@escorial.com.ar>
2025-07-03 15:39:17 -0300

    new classes creation

---
+++ ZendeskApiCore/Models/Flag.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZendeskApiCore.Models
+{
+    [Table("flag")]
+    public class Flag
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+        [Column("descripcion")]
+        [StringLength(100)]
+        public string Descripcion { get; set; }
+        [Column("codigo")]
+        [StringLength(20)]
+        public string Codigo { get; set; }
+    }
+}
---
+++ ZendeskApiCore/Models/TrReclamoDto.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+namespace ZendeskApiCore.Models
+{
+    public class TrReclamoDto
+    {
+        public string Nombre  { get; set; }
+        public string NumeroDocumento { get; set; }
+        public string Estado { get; set; }
+        public Flag Flag { get; set; }
+    }
+}
