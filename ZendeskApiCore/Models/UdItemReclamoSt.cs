70aa8cf429d764057d5c36a79b1280ddb790471c
maurinicolas <mlopez@escorial.com.ar>
2025-07-24 16:08:09 -0300

    añadido de clases para ef

---
+++ ZendeskApiCore/Models/UdItemReclamoSt.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZendeskApiCore.Models
+{
+    [Table("ud_item_reclamo_st")]
+    public class UdItemReclamoSt
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+        [Column("bo_owner_id")]
+        public Guid BoOwnerId { get; set; }
+    }
+}
